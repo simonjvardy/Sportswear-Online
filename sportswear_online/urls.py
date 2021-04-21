babb7d959472bb230d4c86db12b20e3d72358d60
simonjvardy <sjvardy@gmail.com>
2021-04-21 21:40:27 +0000

    Dev: update homepage header & base.css

--- sportswear_online/urls.py
+++ sportswear_online/urls.py
@@ -1,5 +1,7 @@
 """sportswear_online URL Configuration
 
+code adapted from the Code Institute Boutique Ado mini project
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -15,9 +17,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
