fbedfdd4d678f32b3f7cece648bb31a3cbe30e6f
simonjvardy <sjvardy@gmail.com>
2021-04-27 18:55:42 +0000

    Dev: update fictures JSON & build Products models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,93 @@
 from django.db import models
 
-# Create your models here.
+
+class Gender(models.Model):
+    name = models.CharField(
+        max_length=254)
+    display_name = models.CharField(
+        max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def gender_display_name(self):
+        return self.display_name
+
+
+class ArticleType(models.Model):
+    name = models.CharField(
+        max_length=254)
+    display_name = models.CharField(
+        max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def article_type_display_name(self):
+        return self.display_name
+
+
+class MasterCategory(models.Model):
+    name = models.CharField(
+        max_length=254)
+    display_name = models.CharField(
+        max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def master_category_display_name(self):
+        return self.display_name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(
+        max_length=254)
+    display_name = models.CharField(
+        max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def sub_category_display_name(self):
+        return self.display_name
+
+
+class Product(models.Model):
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2)
+    discount_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2)
+    sku = models.CharField(
+        max_length=254)
+    name = models.CharField(
+        max_length=254)
+    product_description = models.TextField()
+    gender = models.ForeignKey(
+        'Gender',
+        null=True,
+        Blank=True,
+        on_delete=models.SET_NULL)
+    master_category = models.ForeignKey(
+        'MasterCategory',
+        null=True,
+        Blank=True,
+        on_delete=models.SET_NULL)
+    sub_category = models.ForeignKey(
+        'SubCategory',
+        null=True,
+        Blank=True,
+        on_delete=models.SET_NULL)
+    article_type = models.ForeignKey(
+        'ArticleType',
+        null=True,
+        Blank=True,
+        on_delete=models.SET_NULL)
+    image = models.ImageField(
+        null=True,
+        blank=True)
+
+    def __str__(self):
+        return self.product_description
