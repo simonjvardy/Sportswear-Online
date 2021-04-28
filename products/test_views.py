1af1e9484263958c9cb99e4c1cda982725c30a5b
simonjvardy <sjvardy@gmail.com>
2021-04-28 22:16:46 +0000

    TDD: Add Products test_views and test_models files

--- products/test_views.py
+++ products/test_views.py
@@ -1,17 +1,21 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 
 
 class TestProductsViews(TestCase):
     """
     Tests the products page renders the correct page
+    Tests the product_page redirects correctly
     """
+
     def test_all_products_page(self):
-        response = self.client.get('/products/', follow=True)
+        response = self.client.get('/products', follow=True)
         self.assertEqual(response.status_code, 200)
         self. assertTemplateUsed(response, 'products/products.html')
 
     def test_product_page(self):
-        response = self.client.get('/products', follow=True)
-        self.assertRedirects(response, '/products/', status_code=301, target_status_code=200)
+        response = self.client.get('/products')
+        self.assertRedirects(
+            response, '/products/', status_code=301, target_status_code=200)
