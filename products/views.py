4fb084cec1fb037d068500724befad85145811b6
simonjvardy <sjvardy@gmail.com>
2021-05-01 21:27:16 +0000

    Dev: Add product sorting functionality

--- products/views.py
+++ products/views.py
@@ -17,6 +17,8 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    sort = None
+    direction = None
     query = None
     gender = None
     master_category = None
@@ -24,33 +26,53 @@ def all_products(request):
     article_type = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lowername=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'gender' in request.GET:
             gender = request.GET['gender'].split(',')
-            products = products.filter(gender__name__in=gender)
-            gender = Gender.objects.filter(name__in=gender)
+            products = products.filter(
+                gender__name__in=gender)
+            gender = Gender.objects.filter(
+                name__in=gender)
 
         if 'master_category' in request.GET:
             master_category = request.GET['master_category'].split(',')
-            products = products.filter(master_category__name__in=master_category)
+            products = products.filter(
+                master_category__name__in=master_category)
             master_category = MasterCategory.objects.filter(
                 name__in=master_category)
 
         if 'sub_category' in request.GET:
             sub_category = request.GET['sub_category'].split(',')
-            products = products.filter(sub_category__name__in=sub_category)
-            sub_category = SubCategory.objects.filter(name__in=sub_category)
+            products = products.filter(
+                sub_category__name__in=sub_category)
+            sub_category = SubCategory.objects.filter(
+                name__in=sub_category)
 
         if 'article_type' in request.GET:
             article_type = request.GET['article_type'].split(',')
-            products = products.filter(article_type__name__in=article_type)
-            article_type = ArticleType.objects.filter(name__in=article_type)
+            products = products.filter(
+                article_type__name__in=article_type)
+            article_type = ArticleType.objects.filter(
+                name__in=article_type)
 
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "Please enter a seearch criteria")
                 return redirect(reverse('products'))
-            
+
             queries = (
                 Q(name__icontains=query) |
                 Q(product_description__icontains=query) |
@@ -60,6 +82,7 @@ def all_products(request):
                 Q(article_type__name__icontains=query))
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
     context = {
         'products': products,
         'search_term': query,
@@ -67,6 +90,7 @@ def all_products(request):
         'current_master_category': master_category,
         'current_sub_category': sub_category,
         'current_article_type': article_type,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
