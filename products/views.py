6124c14aa18de87cf44539731eaa5005769b3cda
simonjvardy <sjvardy@gmail.com>
2021-05-02 22:42:08 +0000

    Fix: Issue #20 Sorting by Name error

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import (
     render, redirect, reverse, get_object_or_404)
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import (
     Product, Gender, MasterCategory, SubCategory, ArticleType)
 
@@ -31,7 +32,7 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lowername=Lower('name'))
+                products = products.annotate(lower_name=Lower('name'))
             
             if 'direction' in request.GET:
                 direction = request.GET['direction']
