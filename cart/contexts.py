12887f3c6483f6358e2c12da2ee6abc40ce5786a
simonjvardy <sjvardy@gmail.com>
2021-05-03 15:28:15 +0000

    Dev: Update shopping cart context processor and views

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,12 +7,25 @@ available to all templates across the entire application
 
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.discount_price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
