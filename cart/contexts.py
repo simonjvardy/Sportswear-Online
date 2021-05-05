70426a02d31ab949e40726cc9da47868522ebc0e
simonjvardy <sjvardy@gmail.com>
2021-05-05 17:32:09 +0000

    Dev: Update shopping cart product sizes logic

--- cart/contexts.py
+++ cart/contexts.py
@@ -17,18 +17,35 @@ def cart_contents(request):
     product_count = 0
     cart = request.session.get('cart', {})
 
-    for item_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.discount_price
-        product_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.discount_price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.discount_price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+
+        # Check if delivery is more than the minimum delivery cost
+        if ((total / Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)) < Decimal(settings.MINIMUM_DELIVERY_COST)):
+            delivery = Decimal(settings.MINIMUM_DELIVERY_COST)
+        else:
+            delivery = total / Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
