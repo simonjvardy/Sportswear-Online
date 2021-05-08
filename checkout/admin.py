437ce8f3131e0eade7b6d922be5972e425d3589e
simonjvardy <sjvardy@gmail.com>
2021-05-08 18:16:15 +0000

    Dev: Build checkout admin.py and strat signals.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,79 @@
-from django.contrib import admin
+# Code adapted from CI Boutique Ado mini project
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order, OrderLineItem
+
+
+class OderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = (
+        'lineitem_total',
+    )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OderLineItemAdminInline,)
+
+    readonly_fields = (
+        'order_number',
+        'order_date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+
+    fieldsets = (
+        ('Order Details', {
+            'fields': (
+                'order_number',
+                'order_date'
+            )
+        }),
+        ('Customer', {
+            'fields': (
+                'first_name',
+                'last_name'
+            )
+        }),
+        ('Customer Details', {
+            'fields': (
+                'email',
+                'telephone'
+            )
+        }),
+        ('Customer Address', {
+            'fields': (
+                'address_line1',
+                'address_line2',
+                'town_or_city',
+                'county',
+                'postcode',
+                'country'
+            )
+        }),
+        ('Order Totals', {
+            'fields': (
+                'delivery_cost',
+                'order_total',
+                'grand_total'
+            )
+        }),
+
+    )
+
+    list_display = (
+        'order_number',
+        'order_date',
+        'first_name',
+        'last_name',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+    )
+
+    ordering = (
+        '-order_date',
+    )
+
+
+admin.site.register(Order, OrderAdmin)
