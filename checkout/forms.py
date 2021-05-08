738720a9fe7cfbd217e864d58b328ac422465989
simonjvardy <sjvardy@gmail.com>
2021-05-08 22:26:03 +0000

    Dev: Add checkout forms, admin and signals

---
+++ checkout/forms.py
@@ -0,0 +1,50 @@
+# Code adapted from CI Boutique Ado mini project
+
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'telephone',
+            'address_line1',
+            'address_line2',
+            'town_or_city',
+            'county',
+            'postcode',
+            'country',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+            'telephone': 'Telephone Number',
+            'address_line1': 'Address Line 1',
+            'address_line2': 'Address Line 2',
+            'town_or_city': 'Town or City',
+            'county': 'County',
+            'postcode': 'Postcode',
+            'country': 'Country',
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
