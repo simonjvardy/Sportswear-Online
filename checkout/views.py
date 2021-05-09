e78b0777f56418b99335e061ab12187751e688df
simonjvardy <sjvardy@gmail.com>
2021-05-09 18:59:25 +0000

    Dev: Install Stripe and update checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -3,10 +3,16 @@
 from django.shortcuts import (
     render, redirect, reverse)
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from cart.contexts import cart_contents
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(
@@ -15,12 +21,28 @@ def checkout(request):
         )
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+
+    # stripe requires the value as an integer as a zero-decimal currency
+    # e.g. £4.99 = 499p
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing. Please set it in your environment.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IcvcUHJhe50oHXbOfXzaUFdrGPP9kotW4kN7g3vPWPVvQRRefjPAujUlGcYHO3UZydz8I56L0i98jxID1pm51vl00uwbspCEF',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
