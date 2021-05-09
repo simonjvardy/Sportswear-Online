fbbc026e48baa29d375c960215303ab23472fdbd
simonjvardy <sjvardy@gmail.com>
2021-05-09 18:30:12 +0000

    Dev: Set up initial Stripe card elements

--- checkout/views.py
+++ checkout/views.py
@@ -19,6 +19,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IcvcUHJhe50oHXbOfXzaUFdrGPP9kotW4kN7g3vPWPVvQRRefjPAujUlGcYHO3UZydz8I56L0i98jxID1pm51vl00uwbspCEF',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
