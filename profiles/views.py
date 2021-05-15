775757043edf686f6159eb96d884cbffb736066f
simonjvardy <sjvardy@gmail.com>
2021-05-15 10:36:37 +0000

    Dev: Update Profile template, views and base.css

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,18 @@
 # Code adapted from CI Boutique Ado mini project
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def profile(request):
-    """ Display the user's profile. """
+    """ 
+    Display the user's profile.
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
