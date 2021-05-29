332fedc1d079ee1d3282b7d195a0fc07592b7b26
simonjvardy <sjvardy@gmail.com>
2021-05-29 23:54:04 +0000

    TDD: Refactor test cases files

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class TestCheckoutViews(TestCase):
+    """
+    Tests the checkout page redirects correctly
+    """
+
+    def test_profile_page(self):
+        response = self.client.get('/checkout', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(
+            response, '/products/', status_code=301, target_status_code=200)
